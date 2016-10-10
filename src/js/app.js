7848085f8fff61bd0a110fe86033c584eb006ceb
Anna Andersson <anna@snigelfart.se>
2016-10-10 20:21:14 +0200

    added axios to make post from save button in addPostItForm and made some changes after group disscussion on whiteboard

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Whiteboard from './components/whiteboard';
-import TitleBar from './components/titlebar';
-import AddPostItButton from './components/addPostItButton';
 
-const Top = React.createClass({
-  render() {
-    return (<div>
-      <TitleBar data={'FLOGGIT WHITEBOARD'} />
-      <AddPostItButton />
-    </div>
-    );
-  }
-});
-
-ReactDOM.render(<Top />, document.querySelector('.header'));
 // ReactDOM.render(top, document.querySelector('.header'));
 ReactDOM.render(<Whiteboard />, document.querySelector('#container'));
-
--- src/js/components/whiteboard.js
+++ src/js/components/whiteboard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import $ from 'jquery';
+import axios from 'axios';
 
 import PostIt from './postit';
+import WhiteboardHeader from './whiteboardHeader';
 
 export default class Whiteboard extends React.Component {
 
@@ -9,30 +11,61 @@ export default class Whiteboard extends React.Component {
     super(props);
     this.apiUrl = 'http://localhost:8080/api/v1/postits';
     this.state = { postIts: [] };
+    this.handleAddPostIt = this.handleAddPostIt.bind(this);
   }
 
   componentDidMount() {
-    // lint fails, how do we bind 'this' when using fat arrow notation =>
-    // (result) => { }.bind(this); fails
-    this.serverRequest = $.get(this.apiUrl, function (result) {
-      console.log(result);
-      this.setState({
-        postIts: this.state.postIts.concat(result)
-      });
-    }.bind(this));
+    axios.get('http://localhost:8080/api/v1/postits').then((response) => {
+      if (response.status === 200) {
+        this.setState({
+          postIts: response.data
+        });
+      }
+    });
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      postIts: nextProps.postIts
+    });
+  }
   componentWillUnmount() {
     this.serverRequest.abort();
   }
 
+  handleAddPostIt(titleInput, description, postItColor) {
+    const postIt = {
+      title: titleInput,
+      text: description,
+      color: postItColor
+    };
+    axios({
+      method: 'post',
+      url: 'http://localhost:8080/api/v1/postits',
+      data: postIt
+    })
+    .then((response) => {
+      if (response.status === 201) {
+        console.log(response);
+        this.setState({
+          postIts: this.state.postIts.concat([{
+            id: response.data.id,
+            item: postIt
+          }])
+        });
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="jumbotron">
-        <div className="post-it panel panel-default">
-
-          {this.state.postIts.map(item => (
-            <PostIt data={item.postIt} />)) }
+      <div>
+        <WhiteboardHeader onAddPostIt={this.handleAddPostIt} />
+        <div className="jumbotron">
+          <div className="post-it panel panel-default">
+            {this.state.postIts.map(item => (
+              <PostIt data={item.postIt} />)) }
+          </div>
         </div>
       </div>
     );
