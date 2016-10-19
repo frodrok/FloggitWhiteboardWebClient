0c8a3bc40216ff3b56fd2c8de1145c256a197d5f
Joanne Nori <joanne_nori@hotmail.com>
2016-10-19 13:24:15 +0200

    socket-code works

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import socketIOclient from 'socket.io-client';
 import * as types from '../constants/action_types';
 
 const internalAddPostIt = postit => ({
@@ -23,6 +24,7 @@ const internalError = errorText => ({
 export const getAll = () => (dispatch) => {
   axios.get('http://localhost:8080/api/v1/postits')
               .then((response) => {
+                console.log('getAll', response.data);
                 dispatch(internalUpdate(response.data));
               });
 };
@@ -30,12 +32,12 @@ export const getAll = () => (dispatch) => {
 export const add = postit => (dispatch) => {
   axios.post('http://localhost:8080/api/v1/postits', postit)
         .then((response) => {
-          const newPostit = {
-            id: response.data.id,
-            postIt: postit
-          };
-          dispatch(internalAddPostIt(newPostit));
-          dispatch(getAll());
+          // const newPostit = {
+          //   id: response.data.id,
+          //   postIt: postit
+          // };
+          // dispatch(internalAddPostIt(newPostit));
+          // dispatch(getAll());
         }).catch((error) => {
           dispatch(internalError('Could not add postit'));
         });
@@ -44,10 +46,21 @@ export const add = postit => (dispatch) => {
 export const remove = id => (dispatch) => {
   axios.delete(`http://localhost:8080/api/v1/postits/${id}`)
         .then((response) => {
-          dispatch(internalRemove(id));
+          // dispatch(internalRemove(id));
         }).catch((error) => {
           dispatch(internalError('Could not remove postit'));
         });
+};
+
+export const startSocket = () => (dispatch) => {
+  console.log('SOCKET STARTED');
+  const socket = socketIOclient('http://localhost:8080');
+
+  socket.on('postit-update', (data) => {
+    console.log('update', data);
+    const postits = data;
+    dispatch(internalUpdate(postits));
+  });
 };
 
 export const showDelete = show => ({
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import { getAll } from '../actions';
+import { getAll, startSocket } from '../actions';
 
 import reducer from '../reducers';
 
 const logger = createLogger();
 const store = createStore(reducer, applyMiddleware(thunk, logger));
-store.dispatch(getAll());
+// store.dispatch(getAll());
+store.dispatch(startSocket());
 export default store;
